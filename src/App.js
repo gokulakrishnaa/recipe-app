196142234426a6aaabba8fc542bf1f9e84c78eec
gokulakrishnaa <gokulakrishnaapsivam@gmail.com>
2021-12-03 23:47:07 +0530

    Recipe with Heroku Api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+const API_URL = "https://node-movieapp.herokuapp.com";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RecipeList />
+    </div>
+  );
+}
+
+function RecipeList() {
+  // const recipes = [];
+  const [recipes, setRecipes] = useState();
+  useEffect(() => {
+    fetch(`${API_URL}/recipes`)
+      .then((data) => data.json())
+      .then((recipes) => setRecipes(recipes));
+  }, []);
+
+  return (
+    <div className="recipe-list-container">
+      {recipes && recipes.length > 0 ? (
+        recipes.map((recipe) => <Recipe recipe={recipe} key={recipe._id} />)
+      ) : (
+        <NoRecipes />
+      )}
+    </div>
+  );
+}
+
+function NoRecipes() {
+  return (
+    <div>
+      <p>No Recipes Available</p>
+    </div>
+  );
+}
+
+function Recipe({ recipe }) {
+  return (
+    <div className="recipe-container">
+      <img className="recipe-picture" src={recipe.image} alt={recipe.name} />
+      <p className="recipe-name">{recipe.name}</p>
     </div>
   );
 }
